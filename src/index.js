c96d97db1268fbb4b71e753b1b1f9ecf88144247
Sergii Podriezienko <podreenko1986@gmail.com>
2023-01-27 13:12:12 +0200

    error message
    error message

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import axios from 'axios';
 
+import Notiflix from 'notiflix';
+
+// one by one
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import { Block } from 'notiflix/build/notiflix-block-aio';
+
+Notiflix.Notify.init({
+  width: '280px',
+  position: 'right-top',
+  // distance: '70px',
+  opacity: 1,
+});
+
 const elemForm = document.querySelector('.search-form');
 const elemInput = document.querySelector('input[name="searchQuery"]');
 
@@ -15,5 +31,18 @@ function onFormSubmit(evt) {
     inputValue +
     '&image_type=photo&orientation=horizontal&safesearch=true';
 
-  const response = axios.get(url);
+  const dateUrl = axios.get(url).then(response => {
+    return response;
+  });
+
+  dateUrl
+    .then(date => {
+      const arrayLength = date.data.hits.length;
+      if (arrayLength === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+    })
+    .catch();
 }
