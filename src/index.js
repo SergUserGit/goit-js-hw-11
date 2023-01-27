c8d047d762d70638a77ab2c432036be0704d85f9
Sergii Podriezienko <podreenko1986@gmail.com>
2023-01-27 12:36:11 +0200

    url
    url

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const elemForm = document.querySelector('.search-form');
 const elemInput = document.querySelector('input[name="searchQuery"]');
 
@@ -5,4 +7,13 @@ elemForm.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(evt) {
   evt.preventDefault();
+
+  const inputValue = elemInput.value;
+
+  const url =
+    'https://pixabay.com/api/?key=33160634-a3b69315a0080f1036a2567f6&q=' +
+    inputValue +
+    '&image_type=photo&orientation=horizontal&safesearch=true';
+
+  const response = axios.get(url);
 }
