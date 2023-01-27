a4c102a5d52d1152f9c5783ed6d17f8bc3ad1b2b
SergUserGit <104759161+SergUserGit@users.noreply.github.com>
2023-01-27 10:36:41 +0000

    Deploying to gh-pages from @ SergUserGit/goit-js-hw-11@c8d047d762d70638a77ab2c432036be0704d85f9 🚀

--- index.577a230d.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".search-form");document.querySelector('input[name="searchQuery"]');e.addEventListener("submit",(function(e){e.preventDefault()}))}();
-//# sourceMappingURL=index.577a230d.js.map
--- index.b2477646.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".search-form");document.querySelector('input[name="searchQuery"]');e.addEventListener("submit",(function(e){e.preventDefault()}));
-//# sourceMappingURL=index.b2477646.js.map
